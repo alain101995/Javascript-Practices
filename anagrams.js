7cbef16ba140b987e6d1966da6e3fdb4a210f8b2
Alain Quiroz <alain101995@gmail.com>
2018-01-29 14:55:05 -0600

    Anagram problem pending

---
+++ anagrams.js
@@ -0,0 +1,9 @@
+/** * Anagrams * Haz un funcion que agrupe e imprima las palabras que sean Anagramas en una lista recibida 
+ * 
+ * Ejemplo: 
+ * Input: 
+ * var words = ["cien", "frase", "acabo", "cine", "abaco", "fresa", "caoba", "nice", "aboca"]; 
+ * Output: 
+ * abaco - acabo - caoba - aboca
+ * cien - cine - nice * frase - fresa 
+ */
